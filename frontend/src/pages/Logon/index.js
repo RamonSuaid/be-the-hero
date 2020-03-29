6d45383ca8ce0e1b54d3319d12fcff40bb0e31e6
Ramon Suaid <ramonsuaid@gmail.com>
2020-03-29 11:08:38 -0300

    Front-End do app

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi';
 import './style.css';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
-return (
-<div className="logon-container">
-    <section className="form">
-        <img src={logoImg} alt="Be The Hero"/>
+    const [id, setID] = useState('');
+    const history=useHistory();
 
-        <form>
+    async function handleLogin(e){
+        e.preventDefault();
+        try {
+            const response = await api.post('sessions', {id});
+            localStorage.setItem('ongID',id);
+            localStorage.setItem('ongName',response.data.name);
+            history.push('/profile');
+
+        }   catch (err) {
+            alert('Falha no login.')
+        }
+    }
+
+return (
+    <div className="logon-container">
+        <section className="form">
+         <img src={logoImg} alt="Be The Hero"/>
+
+         <form onSubmit={handleLogin}>
             <h1>Faça seu Logon</h1>
 
-            <input placeholder="Sua ID"/>
+            <input
+            placeholder="Sua ID"
+            value ={id}
+            onChange = {e => setID(e.target.value)}
+            />
             <button className ="button" type="submit">Entrar</button>        
                 
             <Link className="back-link "to="/register">
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 import logoImg from '../../assets/logo.svg';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
 
+import api from '../../services/api';
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+    
+    const history = useHistory();
+
+    const ongID = localStorage.getItem('ongID');
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+        try {
+            await api.post('incidents', data , {
+                headers: {
+                    Authorization: ongID,
+                }
+            })
+
+            history.push('/profile');
+        }   catch (err) {
+            alert('Erro ao cadastras caso.')
+        }
+    }
     return (
         <div className= "new-incident-container">
         <div className= "content">
@@ -19,12 +48,26 @@ export default function NewIncident() {
                     Voltar para home
                 </Link>
             </section>
-            <form>
-                <input placeholder ="Titulo do caso"/>
-                <textarea placeholder="Descrição"/>
-                <input placeholder ="Valor em reais"/>
+            <form onSubmit={handleNewIncident}>
 
-                <button className="button" type="submit">Cadatrar</button>
+                <input 
+                 placeholder ="Titulo do caso"
+                 value={title}
+                 onChange={e => setTitle(e.target.value)}
+                />
+                <textarea 
+                 placeholder="Descrição"
+                 value={description}
+                 onChange={e => setDescription(e.target.value)}
+
+                />
+                <input 
+                 placeholder ="Valor em reais"
+                 value={value}
+                 onChange={e => setValue(e.target.value)}
+                />
+
+                <button onClick={handleNewIncident} className="button" type="submit">Cadatrar</button>
             </form>
         </div>
     </div>);
